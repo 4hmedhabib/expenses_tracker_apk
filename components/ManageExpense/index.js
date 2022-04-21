17e17310425cb5d5a5789dff0eb26993925b45b2
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-04-22 02:15:15 +0300

    added: Manage expense

---
+++ components/ManageExpense/index.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+//
+import Input from "./Input";
+
+const ManageExpense = () => {
+  return (
+    <View>
+      <Input label="Amount" />
+    </View>
+  );
+};
+
+export default ManageExpense;
+
+const styles = StyleSheet.create({});
